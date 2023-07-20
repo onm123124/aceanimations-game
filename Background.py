2cb3e2dfe94afdc642ef537872265069a5f65c76
onm <omhellyamrut@gmail.com>
2023-07-20 09:19:12 +0530

    q

--- Background.py
+++ Background.py
@@ -1,1 +1,5 @@
-
+import pygame 
+class background:
+    def __init__(self):
+         self.sprite = pygame.image.load('sprites/wdog1s.jpg')
+         self.position = 0
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.math import Vector2
-#more lirary here if need
-
+from Background import background
+#more lirary here if neededs 
+screen = pygame.display.set_mode((1920,1080))
 def main():
     pygame.init()
     pygame.display.set_caption("ACE")
@@ -13,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
+        screen.blit(background().sprite,(0,0))
 
 if __name__ == "__name__":
     main()#run main sequence
