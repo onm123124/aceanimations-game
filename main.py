c5d30f3e1f18c6d5fc963f81f553f4a29cfe8b2d
onm <omhellyamrut@gmail.com>
2023-07-20 08:47:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def main():
     pygame.display.set_caption("ACE")
     #clean screen
     pygame.display.update
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+if __name__ == "__name__":
+    main()#run main sequence
