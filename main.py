89eab57fb888c9e3023e2e27f80a199395212b38
onm <omhellyamrut@gmail.com>
2023-07-20 08:41:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from pygame.math import Vector2
-
+#more lirary here if need
 
 def main():
     pygame.init()
+    pygame.display.set_caption("ACE")
+    #clean screen
+    pygame.display.update
